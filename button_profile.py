aba122ffd210235854f7082807124c8208215611
anish-chapagai <57314527+anish-chapagai@users.noreply.github.com>
2020-04-06 14:25:32 +0545

    Add files via upload

---
+++ button_profile.py
@@ -0,0 +1,27 @@
+import numpy
+players_name = {
+	'player1_name' : 'Player 1',
+	'player2_name' : 'Player 2'
+}
+mode = 0
+game_mode = {1 : ['testhere', mode]}
+
+#current button status
+button_stat = {
+	1:False,
+	2:False,
+	3:False,
+	4:False,
+	5:False,
+	6:False,
+	7:False,
+	8:False,
+	9:False,
+}
+button_stat1 = [
+	[' ',' ',' '],
+	[' ',' ',' '],
+	[' ',' ',' ']
+]
+button_stat1 = numpy.array(button_stat1)
+
